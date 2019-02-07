1f3dfc3500051f883cb23487ae2e3d54e41b385d
standthis <standthis@protonmail.ch>
2019-02-07 16:38:48 +0200

    dh.py complete

--- diffie-hellman/dh.py
+++ diffie-hellman/dh.py
@@ -4,12 +4,14 @@
 
 import sys
 import random 
+import time
 
 if len(sys.argv) != 3: 
     err = sys.argv[0] + " g p"
     print("Expected: " + err)
     sys.exit()
 
+maxSecret = 1000
 g = int(sys.argv[1])
 p = int(sys.argv[2])
 
@@ -18,8 +20,10 @@ print("Publicly Shared Variables:")
 print("Publicly Shared Prime: " + str(g))
 print("Publicly Shared Base: " + str(p))
 
-a = random.randrange(1, 51)
-b = random.randrange(1, 51)
+a = random.randrange(1, maxSecret + 1)
+b = random.randrange(1, maxSecret + 1)
+print(a)
+print(b)
 
 def compute(secret):
     return g**secret % p
@@ -27,18 +31,47 @@ def compute(secret):
 A = compute(a)
 B = compute(b)
 
+print(" Alice secret: " + str(a))
+print(" Bob secret: " + str(b))
 
 print("  Alice Sends Over Public Chanel: " + str(A))
 print("  Bob Sends Over Public Chanel: " + str(B))
 
 def SharedSecret(result, secret):
-    return result ** secret % p
+    return result**secret % p
 
 print("------------")
 print("Privately Calculated Shared Secret:")
+aliceSS = SharedSecret(B, a)
+bobSS = SharedSecret(A,b)
+print("    Alice Shared Secret: " + str(aliceSS))
+print("    Bob Shared Secret: " + str(bobSS))
 
-print("    Alice Shared Secret: " + str(SharedSecret(B, a)))
-print("    Bob Shared Secret: " + str(SharedSecret(A,b)))
+def bruteForce():
+    for i in range(1, maxSecret + 1):
+        if SharedSecret(A, i) == aliceSS:
+            for i in range(1, maxSecret + 1):
+                if bobSS == SharedSecret(B, i):
+                    if aliceSS == bobSS:
+                        return str(aliceSS)
+    return "Failed to brute force"
+
+
+print("\nATTEMPTING BRUCE FORCE BASED ON INTERCEPTION OF NETWORK TRAFFIC")
+t1 = time.time()
+print("The secret key is : " + bruteForce())
+t2 = time.time()
+print("Time taken : " + str(t2 - t1))
+
+
+
+
+
+        
+
+
+
+
 '''
 $ python dh2.py 17 2
 Publicly Shared Variables:
